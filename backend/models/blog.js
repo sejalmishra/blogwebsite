d765670ea7bed552473ed9ddf31431dd8c31289e
sejalmishra <sejalmishra1310@gmail.com>
2023-03-01 19:18:51 +0530

    added blog schema

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -2,10 +2,46 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
     title: String,
-    author: String,
     content: String,
-    dateOfCreation: String
-})
+    dateOfCreation: String,
+    claps: Number,
+    feature_img: String,
+    author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+    },
+    comments: [
+      {
+        author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+    },
+    text: String
+      }
+    ]
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+blogSchema.methods.clap = function() {
+ this.claps++
+  return this.save()
+}
+
+blogSchema.methods.comment = function(c) {
+    this.comments.push(c)
+    return this.save()
+}
+
+blogSchema.methods.addAuthor = function (author_id) {
+    this.author = author_id
+    return this.save()
+}
+
+blogSchema.methods.getUserBlog = async function (_id) {
+  const blog = await Blog.find({'author': _id})
+  return blog
+}
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -15,4 +51,4 @@ blogSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Blog', blogSchema);
+module.exports = Blog
